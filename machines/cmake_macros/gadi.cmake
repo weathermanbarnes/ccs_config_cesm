87ff87fa839778eae912b2d782fa7e9f605dac45
Michael Barnes <39979861+weathermanbarnes@users.noreply.github.com>
2023-12-14 11:41:49 +1100

    Update gadi.cmake

--- machines/cmake_macros/gadi.cmake
+++ machines/cmake_macros/gadi.cmake
@@ -1,1 +1,6 @@
+if (COMP_NAME STREQUAL gptl)
+  string(APPEND CPPDEFS " -DHAVE_NANOTIME -DBIT64 -DHAVE_VPRINTF -DHAVE_BACKTRACE -DHAVE_SLASHPROC -DHAVE_COMM_F2C -DHAVE_TIMES -DHAVE_GETTIMEOFDAY")
+endif()
 
+set(LDFLAGS "")
+string(APPEND SLIBS " -mkl=cluster -lnetcdf -lnetcdff -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -liomp5 -lm")
